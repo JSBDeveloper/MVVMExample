8591b6a60d4c42418754a68e84eaded6cf9f723d
JSBDeveloper <ids8600@naver.com>
2021-06-08 23:48:33 +0900

    update MainActivity

--- app/src/main/java/com/example/mvvmexample/MainActivity.kt
+++ app/src/main/java/com/example/mvvmexample/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.example.mvvmexample
 
+import com.example.mvvmexample.adapter.MainSearchRecyclerViewAdapter
 import com.example.mvvmexample.databinding.ActivityMainBinding
 import com.example.mvvmexample.model.MainViewModel
+import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : BaseView<ActivityMainBinding, MainViewModel>() {
     override val layoutResourceId: Int
         get() = R.layout.activity_main
     override val viewModel: MainViewModel by viewModel()
+
+    private val mainSearchRecyclerViewAdapter: MainSearchRecyclerViewAdapter by inject()
 
     override fun initStartView() {
 
--- app/src/main/java/com/example/mvvmexample/adapter/MainSearchRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/mvvmexample/adapter/MainSearchRecyclerViewAdapter.kt
@@ -1,4 +1,5 @@
 package com.example.mvvmexample.adapter
 
 class MainSearchRecyclerViewAdapter {
+
 }
