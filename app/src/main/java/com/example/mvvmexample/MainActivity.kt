9f069f743c892278db31c4404beaba699d4dff7d
JSBDeveloper <ids8600@naver.com>
2021-06-09 00:21:09 +0900

    apply LiveData

--- app/src/main/java/com/example/mvvmexample/MainActivity.kt
+++ app/src/main/java/com/example/mvvmexample/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.mvvmexample
 
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.mvvmexample.adapter.MainSearchRecyclerViewAdapter
 import com.example.mvvmexample.databinding.ActivityMainBinding
 import com.example.mvvmexample.model.MainViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -14,14 +17,29 @@ class MainActivity : BaseView<ActivityMainBinding, MainViewModel>() {
     private val mainSearchRecyclerViewAdapter: MainSearchRecyclerViewAdapter by inject()
 
     override fun initStartView() {
+        main_activity_search_recycler_view.run {
+            adapter = mainSearchRecyclerViewAdapter
+            layoutManager = StaggeredGridLayoutManager(3, 1).apply {
+                gapStrategy = StaggeredGridLayoutManager.GAP_HANDLING_MOVE_ITEMS_BETWEEN_SPANS
+                orientation = StaggeredGridLayoutManager.VERTICAL
+            }
 
+            setHasFixedSize(true)
+        }
     }
 
     override fun initDataBinding() {
-        TODO("Not yet implemented")
+        viewModel.imageSearchResponseLiveData.observe(this, Observer {
+            it.documents.forEach { document ->
+                mainSearchRecyclerViewAdapter.addImageItem(document.image_url, document.doc_url)
+            }
+            mainSearchRecyclerViewAdapter.notifyDataSetChanged()
+        })
     }
 
     override fun initAfterBinding() {
-        TODO("Not yet implemented")
+        main_activity_search_button.setOnClickListener {
+            viewModel.getImageSearch(main_activity_search_text_view.text.toString(), 1, 80)
+        }
     }
 }
